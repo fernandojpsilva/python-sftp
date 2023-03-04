39ab6958faffd4ebbc79128086d2ea4d2777854c
fernandojpsilva <fjo.psilva@gmail.com>
2023-03-04 00:00:15 +0000

    testing sftp

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pysftp as sftp
 import paramiko
 
 if __name__ == '__main__':
-    cnopts = sftp.CnOpts()
-    cnopts.hostkeys = None
+    cnopts = sftp.CnOpts(knownhosts='resources/known_hosts')
+    #cnopts.hostkeys = None
 
     try:
-        with sftp.Connection(host='localhost', port=8000, username='root', password='', cnopts=cnopts) as sftp:
+        with sftp.Connection(host='test.rebex.net', port=22, username='demo', password='password', cnopts=cnopts) as sftp:
             print("Connected successfuly")
-            sftp.listdir()
+            print(sftp.listdir())
+            sftp.get('readme.txt', preserve_mtime=True)
     except:
         print("error")
