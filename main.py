f9aeb3186313f45382ae7fc4d1fa408ad23286c7
fernandojpsilva <fjo.psilva@gmail.com>
2023-03-07 12:06:03 +0000

    adding connection function

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
-import pysftp as sftp
-import paramiko
+import pysftp
+from urllib.parse import urlparse
+import os
+
+
+class Sftp:
+    def __init__(self, hostname, username, password, port=2222):
+        """Constructor Method"""
+        self.connection = None
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.port = port
+
+    def connect(self):
+        """Connects to SFTP server"""
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None
+
+        try:
+            self.connection = pysftp.Connection(
+                host=self.hostname,
+                username=self.username,
+                password=self.password,
+                port=self.port,
+                cnopts=cnopts
+            )
+        except Exception as err:
+            raise Exception(err)
+        finally:
+            print(f"Connected to {self.hostname} as {self.username}")
+
 
 if __name__ == '__main__':
-    cnopts = sftp.CnOpts(knownhosts='resources/known_hosts')
-    #cnopts.hostkeys = None
+    sftp_url = os.environ.get("SFTPTOGO_URL")
 
-    try:
-        with sftp.Connection(host='test.rebex.net', port=22, username='demo', password='password', cnopts=cnopts) as sftp:
-            print("Connected successfuly")
-            print(sftp.listdir())
-            sftp.get('readme.txt', preserve_mtime=True)
-    except:
-        print("error")
+    parsed_url = urlparse(sftp_url)
+
+    sftp = Sftp(
+        hostname=parsed_url.hostname,
+        username=parsed_url.username,
+        password=parsed_url.password,
+    )
+
+    if not sftp_url:
+        print("First, please set environment variable SFTPTOGO_URL and try again.")
+        exit(0)
+
+    sftp.connect()
